fa35ec8b4f245cb1dcb4cab9e666990e4f237441
Kien <ktn085@ucsd.edu>
2017-02-21 13:31:02 -0800

    Hola

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,27 @@
+package edu.ucsd.cs110.temperature;
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float value = (this.getValue() - 32);
+        value = value * 5;
+        value = value / 9;
+        return new Celsius(value);
+
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return new Fahrenheit(this.getValue());
+    }
+
+    public String toString()
+    {
+        return this.getValue() + " F";
+    }
+}
